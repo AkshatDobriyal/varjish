e85a8993d6568857816b580efa5d55eadd18402a
Akshat Dobriyal <56156498+AkshatDobriyal@users.noreply.github.com>
2021-10-30 17:59:44 +0530

    Updated SearchGym.js

--- src/Components/searchGym/SearchGym.js
+++ src/Components/searchGym/SearchGym.js
@@ -7,6 +7,40 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
 const SearchGym = () => {
+    
+    let token = localStorage.getItem("token");
+
+    const [gyms, setGyms] = useState();
+    
+    const [trainers, setTrainers] = useState();
+
+    const [gymName, setGymName] = useState();
+    const [gymDescription, setGymDescription] = useState();
+
+    const [trainerName, setTrainerName] = useState();
+    const [trainerDescription, setTrainerDescription] = useState();
+
+    useEffect(() => {
+        axios
+            .get(``,
+                {
+                    headers: {
+                        Authorization: `Token ${token}`
+                    }
+                }
+            )
+            .then((res) => {
+                if (res) {
+                    setGyms(res.data)
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+        console.log(gyms);
+
+    }, []);
 
     const top100Films = [
         { label: 'The Shawshank Redemption', year: 1994 }
@@ -17,7 +51,7 @@ const SearchGym = () => {
             <Autocomplete
                 disablePortal
                 id="combo-box-demo"
-                options={top100Films}
+                options={gyms}
                 sx={{ width: 300 }}
                 renderInput={(params) => <TextField {...params} label="Movie" />}
             />
