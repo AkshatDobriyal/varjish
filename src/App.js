d3f41e012bd5cf2ad1ee16938eee77d5b2708af9
AmanKothari20 <amankothari123456@gmail.com>
2021-10-30 16:26:50 +0530

    added dashboard page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {useHistory} from "react-router-dom";
 import { withRouter } from "react-router-dom";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import SignIn from "./Components/Login/SignIn/SignIn";
+import DashboardCard from "./Components/Trainee-dashboard/dashboard_card";
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
 
           <Switch>
             <Route exact path="/"  component={SignIn}/>
-            <Route exact path="/Notebooks"  component={''}/>
+            <Route exact path="/dashboard"  component={DashboardCard}/>
             <Route exact path="/Notes"  component={''}/>
             <Route exact path="/Notebooks/newNotebook" component={''}/>
 
